e002fc45cc691609b6ef21152b60927ddf712e8e
Dmitry Novik <n0vik@yandex-team.ru>
2021-09-17 19:47:54 +0300

    Output memory usage with progress

--- programs/client/Client.cpp
+++ programs/client/Client.cpp
@@ -2551,8 +2551,12 @@ private:
             {
                 thread_times[host_name][thread_id].system_ms = value;
             }
+            else if (event_name == MemoryTracker::USAGE_EVENT_NAME)
+            {
+                thread_times[host_name][thread_id].memory_usage = value;
+            }
         }
-        progress_indication.updateThreadTimes(thread_times);
+        progress_indication.updateThreadEventData(thread_times);
     }
 
 
